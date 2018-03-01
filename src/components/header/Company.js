ea9f52e3a407a65c0f1e7fc09f3ec4adb41b0b03
Embie <embieosman@gmail.com>
2018-03-01 20:33:43 +0200

    c

--- src/components/header/Company.js
+++ src/components/header/Company.js
@@ -5,7 +5,7 @@ class Company extends React.Component {
     render() {
       return (
         <div>
-              <h1 className="hello">Tr<span className="t">a</span>vel H<span className="t">u</span>n<span className="t">t</span></h1>
+              <h1 className="company">Tr<span className="t">a</span>vel H<span className="t">u</span>n<span className="t">t</span></h1>
         </div>
       )
     }
